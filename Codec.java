2b46c2e217492ee15b42dbe62afa55fd84e06e9d
Alisha Tamboli <119527906+alisha-tamboli@users.noreply.github.com>
2024-07-12 20:07:46 +0530

    Codec.java
    Codec

---
+++ Codec.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
+    }
+
+    private void serializeHelper(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("null").append(",");
+        } else {
+            sb.append(node.val).append(",");
+            serializeHelper(node.left, sb);
+            serializeHelper(node.right, sb);
+        }
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> nodes = new LinkedList<>();
+        nodes.addAll(Arrays.asList(data.split(",")));
+        return deserializeHelper(nodes);
+    }
+
+    private TreeNode deserializeHelper(Queue<String> nodes) {
+        String val = nodes.poll();
+        if (val.equals("null")) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = deserializeHelper(nodes);
+        node.right = deserializeHelper(nodes);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        // Example usage:
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+
+        Codec codec = new Codec();
+        String serialized = codec.serialize(root);
+        System.out.println("Serialized tree: " + serialized);
+
+        TreeNode deserialized = codec.deserialize(serialized);
+
+        // Print serialized form of deserialized tree for verification
+        String deserializedSerialized = codec.serialize(deserialized);
+        System.out.println("Deserialized tree: " + deserializedSerialized);
+    }
+}
